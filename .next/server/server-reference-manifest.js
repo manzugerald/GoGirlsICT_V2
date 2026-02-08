96a49fa210d436b6e142cc05bcdffb654bb6ddd7
unknown <manzugerald@gmail.com>
2026-02-08 18:58:48 +0900

    updating old code

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"n0+zfKnBaLz1B3zuVr68V3gYUCsTC5y3isdh6Rk/68E=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yRzIlpLDN+/+nrnbFvdwZnhV/fnJOfKt1H5MZooQ5Wo=\"\n}"
